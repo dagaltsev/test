0ec7e9e1142f48aabf95f654a72cc52b2c39bbb7
david <d.agaltsev@gmail.com>
2025-09-16 18:28:36 +0200

    added functions

--- functions/functions.go
+++ functions/functions.go
@@ -4,6 +4,11 @@ import "fmt"
 
 func main() {
 	PrintGreeting("David")
+	PrintGreeting2("David", 16)
+	PrintGreeting2("Timo", 20)
+	PrintGreeting2("Max", 23)
+	PrintGreeting2("Stani", 45)
+	PrintGreeting2("Milan", 1)
 
 	a := 100
 	b := 200
@@ -13,10 +18,10 @@ func main() {
 	x, y := 5, 10
 	x, y = SwapNumbers(x, y)
 	fmt.Printf("After swapping: x = %d, y = %d\n", x, y)
-}
 
-func PrintGreeting(name string) {
-	fmt.Printf("Hello, %s!\n", name)
+	fmt.Println(ConvertNamesToArray("David", "Timo", "Max"))
+	fmt.Println(ConvertNamesToArray("Stani", "Sabina", "Milan"))
+	fmt.Println(ConvertNamesToArray("Marina", "Irina", "Karina"))
 }
 
 func CalculateSum(a, b int) int {
@@ -28,5 +33,20 @@ func SwapNumbers(x, y int) (int, int) {
 }
 
 // create function to calculate sum for 5 integers
+func CalculateSum2(a, b, c, d, e int) int {
+	return a + b + c + d + e
+}
+
+func PrintGreeting(name string) {
+	fmt.Printf("Hello, %s!\n", name)
+}
 
 // create func to accept name and age and print "Hello <name>, you are <age> years old"
+func PrintGreeting2(name string, age int) {
+	fmt.Printf("Hello %s, you are %d years old\n", name, age)
+}
+
+// create func which accept 3 names and returns array of those names => []string{"David","Timo","Max"}
+func ConvertNamesToArray(name1, name2, name3 string) []string {
+	return []string{name1, name2, name3}
+}
