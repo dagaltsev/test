377a2767185b88b8744ff05bd8be2aafce8b3a59
Stanislav Agaltsev <stanislav.agaltsev@mercedes-benz.com>
2025-09-16 17:34:10 +0200

    Added 2 simple functions

--- functions/functions.go
+++ functions/functions.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	PrintGreeting("David")
+
+	a := 100
+	b := 200
+	sum := CalculateSum(a, b)
+	fmt.Printf("The sum of %d and %d is %d\n", a, b, sum)
+}
+
+func PrintGreeting(name string) {
+	fmt.Printf("Hello, %s!\n", name)
+}
+
+func CalculateSum(a, b int) int {
+	return a + b
 }
