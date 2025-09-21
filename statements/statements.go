3b2810af82b8cd5ab0b89c249895c880500a8fe4
Stanislav Agaltsev <stanislav.agaltsev@mercedes-benz.com>
2025-09-21 11:03:56 +0200

    excersise

--- statements/statements.go
+++ statements/statements.go
@@ -4,6 +4,16 @@ import "fmt"
 
 func main() {
 	CheckName2("Sabina")
+
+	// PrintClass1(10) // "Du gehst in 4te Klasse"
+	// PrintClass1(11) // "Du gehst in 5te Klasse"
+	// PrintClass1(12) // "Du gehst in 6te Klasse"
+	// PrintClass1(13) // "Du gehst in 7te Klasse"
+	// PrintClass1(14) // "Du gehst in 8te Klasse"
+	// PrintClass1(15) // "Du gehst in 9te Klasse"
+	// PrintClass1(16) // "Du gehst in 10te Klasse"
+	// PrintClass1(17) // "Du gehst in 11te Klasse"
+	// PrintClass1(18) // "Du gehst in 12te Klasse"
 }
 
 func CheckName2(name string) {
@@ -59,3 +69,12 @@ func ValidateAge(age int) {
 		fmt.Println("You are allowed to vote")
 	}
 }
+
+// create func
+// which accepts age as int
+// based on age you print class for primary school (age 10 -> "Du gehst in 4te Klasse", etc)
+// if, else if, else
+func PrintClass1(age int) {}
+
+// switch case
+func PrintClass2(age int) {}
