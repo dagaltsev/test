e4b41b8ad0820abe115af1319ecf3737b32e790a
david <d.agaltsev@gmail.com>
2025-09-21 11:42:11 +0200

    added statements

--- statements/statements.go
+++ statements/statements.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	CheckName2("Sabina")
 
-	// PrintClass1(10) // "Du gehst in 4te Klasse"
+	PrintClass2(18) // "Du gehst in 4te Klasse"
 	// PrintClass1(11) // "Du gehst in 5te Klasse"
 	// PrintClass1(12) // "Du gehst in 6te Klasse"
 	// PrintClass1(13) // "Du gehst in 7te Klasse"
@@ -14,6 +14,8 @@ func main() {
 	// PrintClass1(16) // "Du gehst in 10te Klasse"
 	// PrintClass1(17) // "Du gehst in 11te Klasse"
 	// PrintClass1(18) // "Du gehst in 12te Klasse"
+
+	fmt.Println(CompareStrings("David geht in die 10te Klasse", "David geht in die 10te Klasse"))
 }
 
 func CheckName2(name string) {
@@ -74,7 +76,56 @@ func ValidateAge(age int) {
 // which accepts age as int
 // based on age you print class for primary school (age 10 -> "Du gehst in 4te Klasse", etc)
 // if, else if, else
-func PrintClass1(age int) {}
+func PrintClass1(age int) {
+	if age == 10 {
+		fmt.Println("Du gehst in die 4te Klasse")
+	} else if age == 11 {
+		fmt.Println("Du gehst in die 5te Klasse")
+	} else if age == 12 {
+		fmt.Println("Du gehst in die 6te Klasse")
+	} else if age == 13 {
+		fmt.Println("Du gehst in die 7te Klasse")
+	} else if age == 14 {
+		fmt.Println("Du gehst in die 8te Klasse")
+	} else if age == 15 {
+		fmt.Println("Du gehst in die 9te Klasse")
+	} else if age == 16 {
+		fmt.Println("Du gehst in die 10te Klasse")
+	} else if age == 17 {
+		fmt.Println("Du gehst in die 11te Klasse")
+	} else if age == 18 {
+		fmt.Println("Du gehst in die 12te Klasse")
+	} else if age > 18 {
+		fmt.Println("Du gehst nicht mehr zur Schule")
+	}
+}
 
 // switch case
-func PrintClass2(age int) {}
+func PrintClass2(age int) {
+	switch age {
+	case 10:
+		fmt.Println("Du gehst in die 4 Klasse")
+	case 11:
+		fmt.Println("Du gehst in die 5 Klasse")
+	case 12:
+		fmt.Println("Du gehst in die 6 Klasse")
+	case 13:
+		fmt.Println("Du gehst in die 7 Klasse")
+	case 14:
+		fmt.Println("Du gehst in die 8 Klasse")
+	case 15:
+		fmt.Println("Du gehst in die 9 Klasse")
+	case 16:
+		fmt.Println("Du gehst in die 10 Klasse")
+	case 17:
+		fmt.Println("Du gehst in die 11 Klasse")
+	case 18:
+		fmt.Println("Du gehst in die 12 Klasse")
+	default:
+		fmt.Println("Du gehst nicht mehr zur Schule")
+	}
+}
+
+func CompareStrings(given, expected string) bool {
+	return given == expected
+}
