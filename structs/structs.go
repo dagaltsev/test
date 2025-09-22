32440430745db04ec362e321877b3c2892f9d533
david <d.agaltsev@gmail.com>
2025-09-22 17:42:22 +0200

    added structs

--- structs/structs.go
+++ structs/structs.go
@@ -43,5 +43,50 @@ func main() {
 func GeneratePersons() []Person {
 	var persons []Person
 
+	David := Person{
+		Name:     "David",
+		Lastname: "Agaltsev",
+		Age:      16,
+		Email:    "David.agaltsev@example.com",
+	}
+
+	persons = append(persons, David)
+
+	Timo := Person{
+		Name:     "Timo",
+		Lastname: "Agaltsev",
+		Age:      20,
+		Email:    "Timo.agaltsev@example.com",
+	}
+
+	persons = append(persons, Timo)
+
+	Max := Person{
+		Name:     "Max",
+		Lastname: "Agaltsev",
+		Age:      23,
+		Email:    "Max.agaltsev@example.com",
+	}
+
+	persons = append(persons, Max)
+
+	Sabina := Person{
+		Name:     "Sabina",
+		Lastname: "Agaltsev",
+		Age:      44,
+		Email:    "Sabina.agaltsev@example.com",
+	}
+
+	persons = append(persons, Sabina)
+
+	Stani := Person{
+		Name:     "Stani",
+		Lastname: "Agaltsev",
+		Age:      45,
+		Email:    "Stani.agaltsev@example.com",
+	}
+
+	persons = append(persons, Stani)
+
 	return persons
 }
